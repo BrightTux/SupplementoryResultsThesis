544a35aa62dbadaa022bf2bb476fab71dcae0128
Clarence <clarence_han@hotmail.com>
2019-03-25 18:01:58 +0800

    add dtw metrics

--- genDist.py
+++ genDist.py
@@ -1,6 +1,10 @@
 import time
 #import statements
 import numpy as np
+from dtw import dtw
+
+x = np.array([2, 0, 1, 1, 2, 4, 2, 1, 2, 0]).reshape(-1, 1)
+print(x)
 
 #list of trajectories for both gt and query [x,y]
 
@@ -21,32 +25,28 @@ queries = [q1,q2,q3,q4,q5]
 
 gt1=[(15,7),(15,8),(16,9),(16,10),(17,12),(17,13),(16,12),(15,14),(15,13),(14,11),(12,10),(12,12),(11,11),(10,12),(9,11)]
 
-gt2=[(14,13),(15,12),(14,12),(14,11),(12,11),(11,12),(11,10),(10,10),(9,11),(8,12),(6,9),(6,8),(5,11),(3,2),(3,12)]
 
-gt3=[(16,6),(16,7),(16,8),(16,9),(16,10),(16,11),(16,12),(16,13),(15,13),(14,13),(13,13),(13,12),(11,12),(10,12),(10,11),(9,11),(8,11),(7,11),(6,11),(5,11),(5,12),(5,13)]
-
-gt4=[(15,4),(15,5),(15,6),(14,6),(13,6),(12,6),(11,6),(11,7),(10,7),(9,7),(9,8),(8,8),(7,8),(7,9),(6,9),(5,10),(4,10),(3,10),(2,10),(1,11),(0,11)]
-
-gt5=[(16,6),(16,7),(16,8),(17,9),(17,10),(17,11),(17,12),(17,13),(17,14),(17,15),(17,16),(18,17),(18,18),(16,18),(15,18),(14,18),(13,18),(12,18),(11,18),(10,18),(9,18),(9,17),(8,17),(7,17)]
-
-gt6=[(15,6),(16,7),(16,8),(16,9),(16,10),(17,11),(17,12),(17,13),(18,13),(18,14),(19,14),(19,13)]
-
-gt7=[(2,11),(3,11),(3,10),(4,10),(5,10),(5,9),(6,9),(7,9),(7,8),(8,8),(9,8),(9,9),(10,9),(11,9),(12,9),(13,9),(14,9),(15,9),(16,9),(16,10)]
-
-gt8=[(15,5),(15,6),(15,7),(15,8),(16,8),(16,9),(16,10),(17,10),(17,11),(17,12),(17,13),(17,14),(17,15)]
-
-gt9=[(15,6),(16,6),(16,7),(16,8),(16,9),(16,10),(15,10),(14,10),(13,10),(12,10),(11,10),(10,9),(9,9),(8,9),(8,8)]
-
-gt10=[(2,17),(3,17),(4,17),(5,17),(6,17),(8,17),(8,18),(9,18),(10,18),(11,18),(12,18),(13,18),(14,18),(15,18),(15,19)]
-
-gt11=[(15,6),(15,7),(16,7),(16,8),(16,9),(16,10),(17,10),(17,11),(17,12),(17,13),(16,13),(15,13),(14,12),(13,12),(12,12),(11,12),(10,12),(9,12),(8,12),(7,12),(7,11),(6,11),(5,11),(8,9)]
-
-
-
-groundtruths = [gt1,gt2,gt3,gt4,gt5,gt6,gt7,gt8,gt9,gt10,gt11]
+groundtruths = [gt1]
 # ----------------------------------------------- E N D   O F   G R O U N D T R U T H S -------------------------------------------------------------
 
 final_cham=[]
+
+
+def dtwFunction(gt,q):
+
+    x=[(15,7),(15,8),(16,9),(16,10),(17,12),(17,13),(16,12),(15,14),(15,13),(14,11),(12,10),(12,12),(11,11),(10,12),(9,11)]
+
+
+    y=[(16,6),(16,7),(16,8),(17,9),(17,10),(17,11),(17,12),(17,13),(17,14),(17,15),(17,16),(18,17),(18,18),(16,18),(15,18),(14,18),(13,18),(12,18),(11,18),(10,18),(9,18),(9,17),(8,17),(7,17)]
+
+
+    euclidean_norm = lambda x, y: np.abs(x - y)
+    d, cost_matrix, acc_cost_matrix, path = dtw(x, y, dist=euclidean_norm)
+
+    print(d)
+
+
+
 
 
 def chamferDistance2(gt, q):
@@ -58,59 +58,64 @@ def chamferDistance2(gt, q):
     currentIndexOfMin = 0
     maxDifference = 0
 
+    print("length of gt: ", len(gt))
+    print("length of q : ", len(q))
+    print("Query will always be longer than gt. 5 test case, 1 GT")
 
-    if(len(gt) >= len(q)):
-       outerloop = len(gt)
-       innerloop = len(q)
-       outer = gt
-       inner = q
-       print("first")
-    else:
-        outerloop = len(q)
-        innerloop = len(gt)
-        outer = q
-        inner = gt
-        print("second")
+    for indq, val in enumerate(q):
 
-    for oidx, o in enumerate(outer):
-        for iidx, i in enumerate(inner):
+        if(len(gt[0]) >= len(q[indq])):
+            outerloop = len(gt[0])
+            innerloop = len(q[indq])
+            outer = gt[0]
+            inner = q[indq]
+        else:
+            outerloop = len(q[indq])
+            innerloop = len(gt[0])
+            outer = q[indq]
+            inner = gt[0]
 
-            print(o,"next ",i)
+        for oidx, o in enumerate(outer):
+            for iidx, i in enumerate(inner):
 
-            min_x.append(abs(o[0] - i[0]))
-            min_y.append(abs(o[1] - i[1]))
-            min_t.append(abs(oidx - iidx))
+                #print(o, i)
 
-        for k, val in enumerate(min_x):
-            tsum.append(min_x[k] + min_y[k] + min_t[k])
+                min_x.append(abs(o[0] - i[0]))
+                min_y.append(abs(o[1] - i[1]))
+                min_t.append(abs(oidx - iidx))
 
-        #indexOfMin = min(xrange(len(tsum)), key=tsum.__getitem__)
-        new_tsum=np.array(tsum)
-        indexOfMin = np.where(new_tsum==new_tsum.min())
+            for k, val in enumerate(min_x):
+                tsum.append(min_x[k] + min_y[k] + min_t[k])
 
-        if(currentIndexOfMin == 0):
-            ModifiedMinValue = new_tsum.min()
+            #indexOfMin = min(xrange(len(tsum)), key=tsum.__getitem__)
+            new_tsum=np.array(tsum)
+            indexOfMin = np.where(new_tsum==new_tsum.min())
 
-        maxDifference = maxDifference + ModifiedMinValue
+            if(currentIndexOfMin == 0):
+                ModifiedMinValue = new_tsum.min()
 
-        min_x = []
-        min_y = []
-        min_t = []
-        tsum = []
+            maxDifference = maxDifference + ModifiedMinValue
 
-
-    cDistance = round(((pow(maxDifference,2))/outerloop)*100.0)/100.0
+            min_x = []
+            min_y = []
+            min_t = []
+            tsum = []
 
 
-    curveAdjuster = 300
-    similarityScore = round((1.0-((cDistance)/(cDistance + curveAdjuster)))*100.0)
+        cDistance = round(((pow(maxDifference,2))/outerloop)*100.0)/100.0
+
+
+        curveAdjuster = 300
+        similarityScore = round((1.0-((cDistance)/(cDistance + curveAdjuster)))*100.0)
 
     #print("similarity score: ", similarityScore)
     #print("cDistance: ", cDistance)
     #time.sleep( 2 )
 
     #print('***************************************')
-    final_cham.append([similarityScore, cDistance])
+        print("Similarity score: ", similarityScore)
+        print("cDistance:        ", cDistance)
+        final_cham.append([similarityScore, cDistance])
 
     return final_cham
 
@@ -124,12 +129,5 @@ hausdrorff = []
 
 
 print(chamferDistance2(groundtruths, queries))
-
-for idx, val in enumerate(groundtruths):
-    #print(idx, '-------------------------------------------')
-    chamferDistance(groundtruths[idx],queries[idx])
-    if(idx ==10):
-        print("chamfer distance: ", chamferDistance(groundtruths[idx],queries[idx]))
-
-
+dtwFunction(groundtruths, queries)
 #https://stackoverflow.com/questions/30706079/hausdorff-distance-between-3d-grids
